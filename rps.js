454aa7fbba772756a4d6a38bc9da52a18bfbb77d
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-10-20 17:32:55 -0300

    Move javascript events to external file

--- rps.js
+++ rps.js
@@ -1,37 +1,51 @@
-let pc_score = 0
-let user_score = 0
-const rps = ['Rock', 'Paper', 'Scissor'] 
+let pc_score = 0;
+let user_score = 0;
+const rps = ['Rock', 'Paper', 'Scissor'];
 
-function randomNumber () {
+// Get options
+const choice = document.getElementsByClassName('u-choice')
+// Add click check for each button
+for (var i = 0; i < choice.length; i++) {
+    choice[i].addEventListener("click", function () {
+        // Click check will call the player_choice function with the button id as parameter
+        player_choice(this.id);
+    });
+}
+
+function randomNumber() {
     return Math.floor(Math.random() * 3);
 }
 
-function player_choice (option)  {
-    let pc_choice = randomNumber ();
+function player_choice(option) {
+    const round = document.getElementById('round')
+    let pc_choice = randomNumber();
     document.getElementById('picks').innerHTML = `You picked <strong>${rps[option]}</strong>, PC Picked <strong>${rps[pc_choice]}</strong>`;
-    if (option == pc_choice) {
-        document.getElementById('round').innerHTML = 'Tie';
+    if (option < 0 || option > 2 && typeof option !== 'number') {
+        player_choice(randomNumber())
+    }
+    else if (option == pc_choice) {
+        round.innerHTML = 'Tie';
     }
     else if (option == 0 && pc_choice == 2) {
         ++user_score;
-        document.getElementById('round').innerHTML = '<strong>You</strong> won the round';
+        round.innerHTML = '<strong>You</strong> won the round';
     }
     else if (option == 1 && pc_choice == 0) {
         ++user_score;
-        document.getElementById('round').innerHTML = '<strong>You</strong> won the round';
+        round.innerHTML = '<strong>You</strong> won the round';
     }
     else if (option == 2 && pc_choice == 1) {
         ++user_score;
-        document.getElementById('round').innerHTML = '<strong>You</strong> won the round';
+        round.innerHTML = '<strong>You</strong> won the round';
     }
     else {
         ++pc_score;
-        document.getElementById('round').innerHTML = '<strong>PC</strong> won the round';
+        round.innerHTML = '<strong>PC</strong> won the round';
     }
-    changeScore();
+    checkWinner();
 }
 
-function changeScore () {
+function checkWinner() {
     if (user_score == 5) {
         pc_score = 0;
         user_score = 0;
